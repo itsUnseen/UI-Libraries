3e677eb7bda00163117f387ab4f88d7d66cb6c37
itsUnseen <97067810+itsUnseen@users.noreply.github.com>
2023-02-02 22:48:22 -0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -24,7 +24,7 @@ repeat wait() until game:IsLoaded()
 		getgenv().library:Unload()
 	end
 
-	local library = {design = getgenv().design == "kali" and "kali" or "cware | version " .. version, tabs = {}, draggable = true, flags = {}, title = "cware | version " .. version, open = false, mousestate = inputService.MouseIconEnabled, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = "uw_configs", fileext = ".uw"}
+	local library = {design = getgenv().design == "kali" and "kali" or "cware | premium user", tabs = {}, draggable = true, flags = {}, title = "cware | version " .. version, open = false, mousestate = inputService.MouseIconEnabled, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = "uw_configs", fileext = ".uw"}
 	getgenv().library = library
 
 	--Locals
